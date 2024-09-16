9daa30c4e9414a462dbd1b429ec124f2db3fe494
Sangeetha <sangeethasenu@gmail.com>
2024-09-16 23:18:03 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+	
+		float versionOfType =91.0f;
+		String developerOfType = "Google";
+		boolean isBetaOfType =false;
+		int releaseYearOfType =2008;
+		char shortcutKeyOfType ='C';
+		
+		System.out.println ("Version of Type :"+versionOfType);
+		System.out.println ("Developer Of Type : "+developerOfType);
+		System.out.println ("Is Beta oF Type : "+isBetaOfType);
+		System.out.println ("Release Year Of Type : "+releaseYearOfType);
+		System.out.println ("Short Cut Key Of Type : "+shortcutKeyOfType);
+		
+				
+
+	}
+
+}
