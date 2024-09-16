ab751713dbdf7d4decda75dfdd578cd8aca8669f
Sangeetha <sangeethasenu@gmail.com>
2024-09-16 22:50:47 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		int num=10;
+		if(num>0)  {
+			
+		System.out.println (num+" is Positive Number");
+		}
+		else if(num<0)   {
+			
+			System.out.println (num+" is Negative Number");
+		}
+	}
+
+}
